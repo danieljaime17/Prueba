eece19690694a96dcc4a4b379aa144de5a40fca7
Danijaime17 <danijaime17@gmail.com>
2022-03-18 00:19:08 +0100

    Segundo commit de prueba

---
+++ Ejercicio1.py
@@ -0,0 +1,9 @@
+print("Hola mundo \n que tal")
+
+print("Hello\nWorld") # Notice the use of \n
+print("My\nName\nIs\nPython") # Notice the use of \n
+
+a = [1, 2, 3, 4, 5]
+
+for x in a:
+    print(x, end = "\n") # \n is append to the end of the string 
