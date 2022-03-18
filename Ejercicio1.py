57e83ae39e6560bacc547c15e1ab3d50c776cc78
Danijaime17 <danijaime17@gmail.com>
2022-03-18 16:18:07 +0100

    Cuarto commit

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,7 +3,7 @@ print("Hola mundo \n que tal")
 print("Hello\nWorld") # Notice the use of \n
 print("My\nName\nIs\nPython") # Notice the use of \n
 
-a = [1, 2, 3, 4, 5, 6]
+a = [1, 2, 3, 4, 5, 6, 7],
 
 for x in a:
     print(x, end = "\n") # \n is append to the end of the string 
